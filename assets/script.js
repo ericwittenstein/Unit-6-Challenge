3659f963710e42859ff6e45ced49b839b752de39
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-07-06 13:47:55 -0400

    got the icon to show up, got the uv index to work, now just need to get the forecast done

--- assets/script.js
+++ assets/script.js
@@ -5,20 +5,86 @@ var buttonEl = $('#button');
 var todayTemp = $('#currentTemp');
 var todayWind = $('#currentWind');
 var todayHumid = $('#currentHumid');
-var todayUV = $('#currentUV');
-// var todayDateEl = $('#currentDate');
+var todayUV = $('#currentUVIndex');
+var todayIcon = $('#currentIcon');
 var CityEl = $('#displayCity');
-
-// var searchedCityData = new [];
 
 // use moment to set current date and 5 days ahead
 var todayDateEl = $('#currentDate').append(moment().format("MM/DD/YYYY"));
-var date1 = $('#day1Date').append(moment().add(1,'days').format("MM/DD/YYYY"));
-var dat2 = $('#day2Date').append(moment().add(2,'days').format("MM/DD/YYYY"));
-var date3 = $('#day3Date').append(moment().add(3,'days').format("MM/DD/YYYY"));
-var date4 = $('#day4Date').append(moment().add(4,'days').format("MM/DD/YYYY"));
-var date5 = $('#day5Date').append(moment().add(5,'days').format("MM/DD/YYYY"));
+var date1 = $('#day1Date').append(moment().add(1, 'days').format("MM/DD/YYYY"));
+var dat2 = $('#day2Date').append(moment().add(2, 'days').format("MM/DD/YYYY"));
+var date3 = $('#day3Date').append(moment().add(3, 'days').format("MM/DD/YYYY"));
+var date4 = $('#day4Date').append(moment().add(4, 'days').format("MM/DD/YYYY"));
+var date5 = $('#day5Date').append(moment().add(5, 'days').format("MM/DD/YYYY"));
 
+// function to call the API using the requested city to extract the lat and long
+function getTodayCoords(cityName) {
+    var callWeatherPt1 = 'https://api.openweathermap.org/data/2.5/weather?q=';
+    var callWeatherPt2 = cityName;
+    var callWeatherPt3 = '&units=imperial&appid=4cd991bb843c8dfb86a480140e987fb5';
+    var callWeatherForLatLong = callWeatherPt1 + callWeatherPt2 + callWeatherPt3;
+
+    // need to extract the latitude and longitude for the data
+    fetch(callWeatherForLatLong).then(function (response) {
+        return response.json()
+    })
+        .then(function (data) {
+            // set main city name display
+            var todayCityVal = data['name'];
+            CityEl.replaceWith('<h3 id="displayCity">' + todayCityVal.toUpperCase() + '</h3>');
+            
+            console.log('initial pull for latlong', data);
+            var latVal = data['coord']['lat'];
+            var lonVal = data['coord']['lon'];
+            getWeatherWithCoords(latVal, lonVal);
+        });
+}
+
+// create new call link with lat lon values
+function getWeatherWithCoords(lat, lon) {
+    var callWeather = 'https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&units=imperial&appid=4cd991bb843c8dfb86a480140e987fb5';
+
+    // fetched the info from the api call and handles it through json
+    fetch(callWeather).then(function (response) {
+        return response.json()
+    })
+        // assignment of current day's weather info to their respective html fields
+        .then(function (data) {
+            console.log('second pull', data);
+
+            // set icon display
+            var todayIconVal = data['current']['weather']['0']['icon'];
+            var todayIconURL = 'http://openweathermap.org/img/wn/' + todayIconVal + '@2x.png';
+            todayIcon.append('<img src="' + todayIconURL + '" />');
+
+            // set main temp display
+            var todayTempVal = data['current']['temp'];
+            todayTemp.append(todayTempVal + " °F");
+
+            // set main wind speed display
+            var todayWindVal = data['current']['wind_speed'];
+            todayWind.append(todayWindVal + " MPH");
+
+            // set main humidity display
+            var todayHumidVal = data['current']['humidity'];
+            todayHumid.append(todayHumidVal + '% (feels like ' + data['current']['feels_like'] + '°F)');
+
+            // set main UV index value
+            var todayUvVal = data['current']['uvi'];
+            if (todayUvVal < 5.99) {
+                todayUV.addClass("greenUV");
+            }
+            else if (todayUvVal > 6 && todayDateEl < 7.99) {
+                todayUV.addClass("yellowUV");
+            }
+            else if (todayUvVal > 8) {
+                todayUV.addClass("redUV");
+            }
+            todayUV.append(todayUvVal);
+        });
+}
+
+// event listeners for the save button
 // function to save to local storage
 buttonEl.on('click', function (event) {
     // prevents a default
@@ -28,17 +94,7 @@ buttonEl.on('click', function (event) {
     // saves that variable to local storage
     localStorage.setItem('new city', searchedCityNameEl);
     // creates a new list item with that city from local storage
-    savedCitiesEl.append('<li class="list-group-item">' + localStorage.getItem('new city') + '</li>');
-    $("#displayCity").replaceWith('<h3 id="displayCity">' + searchedCityNameEl + '</h3>');
+    savedCitiesEl.append('<li class="list-group-item">' + localStorage.getItem('new city').toUpperCase() + '</li>');
+    // $("#displayCity").replaceWith('<h3 id="displayCity">' + searchedCityNameEl + '</h3>');
+    getTodayCoords(searchedCityNameEl);
 });
-
-// function to call the API using the requested city
-function getTodayWeather() {
-    var callWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + localStorage.getItem('new city') + '&units=imperial&appid=b0572ae39a1b4f96f4d79c380369edb3';
-    
-    console.log(callWeather);
-}
-
-// event listeners for the save button
-buttonEl.click(getTodayWeather());
-// buttonEl.click(get5DayWeather());
