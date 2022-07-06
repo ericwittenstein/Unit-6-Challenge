09e9e56c85183a599af817662f41d385ed7f38c0
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-07-06 01:13:47 -0400

    got the layout sorted, fixed save to local storage, fixed adding recently searched to list, fixed initial API pull for weather data, still need to get linking sorted out, plus call for 5 day weather and display of weather data to page

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,46 @@
-var searchBarEl = document.getElementById('#searchBar');
+// initialize variables from index
+// var searchedCityNameEl = $('#searchBar');
+var savedCitiesEl = $('#savedCities');
+var buttonEl = $('#button');
+var todayTemp = $('#currentTemp');
+var todayWind = $('#currentWind');
+var todayHumid = $('#currentHumid');
+var todayUV = $('#currentUV');
+// var todayDateEl = $('#currentDate');
+var CityEl = $('#displayCity');
+
+// var searchedCityData = new [];
+
+// use moment to set current date and 5 days ahead
+var todayDateEl = $('#currentDate').append(moment().format("MM/DD/YYYY"));
+var date1 = $('#day1Date').append(moment().add(1,'days').format("MM/DD/YYYY"));
+var dat2 = $('#day2Date').append(moment().add(2,'days').format("MM/DD/YYYY"));
+var date3 = $('#day3Date').append(moment().add(3,'days').format("MM/DD/YYYY"));
+var date4 = $('#day4Date').append(moment().add(4,'days').format("MM/DD/YYYY"));
+var date5 = $('#day5Date').append(moment().add(5,'days').format("MM/DD/YYYY"));
+
+// function to save to local storage
+buttonEl.on('click', function (event) {
+    // prevents a default
+    event.preventDefault();
+    // creates a variable that pulls in the input from the search bar
+    var searchedCityNameEl = $(this).siblings("input").val();
+    // saves that variable to local storage
+    localStorage.setItem('new city', searchedCityNameEl);
+    // creates a new list item with that city from local storage
+    savedCitiesEl.append('<li class="list-group-item">' + localStorage.getItem('new city') + '</li>');
+    $("#displayCity").replaceWith('<h3 id="displayCity">' + searchedCityNameEl + '</h3>');
+});
+
+// openweather api key: b0572ae39a1b4f96f4d79c380369edb3
+
+// function to call the API using the requested city
+function getTodayWeather() {
+    var callWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + localStorage.getItem('new city') + '&units=imperial&appid=b0572ae39a1b4f96f4d79c380369edb3';
+    
+    console.log(callWeather);
+}
+
+// event listeners for the save button
+buttonEl.click(getTodayWeather());
+// buttonEl.click(get5DayWeather());
